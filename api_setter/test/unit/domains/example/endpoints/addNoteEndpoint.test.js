5c56531b95528782c2e6f8cfabbac9fad61c98be
MIchal Burdzy <mmicalt@gmail.com>
2020-03-17 20:47:08 +0100

    feat: travis config

--- api_setter/test/unit/domains/example/endpoints/addNoteEndpoint.test.js
+++ api_setter/test/unit/domains/example/endpoints/addNoteEndpoint.test.js
@@ -1,24 +1,29 @@
-const AddNoteEndpoint = require('../../../../../src/domains/notes/endpoints/addNoteEndpoint');
+const AddNoteEndpoint = require("../../../../../src/domains/notes/endpoints/addNoteEndpoint");
 
 const fakeLogger = {
   info: jest.fn()
-}
+};
 
-describe('addNoteEndpoint', () => {
-  describe('process', () => {
-    test('runs correctly', async () => {
+describe("addNoteEndpoint", () => {
+  describe("process", () => {
+    test("runs correctly", async () => {
       const addNoteService = {
-        process: jest.fn().mockReturnValue('ok'),
+        process: jest.fn().mockReturnValue("ok")
       };
-      const endpoint = new AddNoteEndpoint({ service: addNoteService, logger: fakeLogger });
-      const result = await endpoint.process({body: {
-        note: 'note'
-      }});
+      const endpoint = new AddNoteEndpoint({
+        service: addNoteService,
+        logger: fakeLogger
+      });
+      const result = await endpoint.process({
+        body: {
+          note: "note"
+        }
+      });
 
-      expect(result.body).toBe('ok');
+      expect(result.body.message).toBe("ok");
       expect(fakeLogger.info).toBeCalled();
       expect(addNoteService.process).toBeCalledWith({
-        note: 'note'
+        note: "note"
       });
     });
   });
