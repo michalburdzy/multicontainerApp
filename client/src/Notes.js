6cdcc7187b0310d9e9d9b985575bde9a6e59e925
MIchal Burdzy <mmicalt@gmail.com>
2020-03-20 20:15:35 +0100

    fix: client app if no api available

--- client/src/Notes.js
+++ client/src/Notes.js
@@ -1,67 +1,70 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Notes  = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [isSubmitting, setIsSubmitting] = useState(false)
+const Notes = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const fetchNotes = async () => {
     try {
-      const response = await axios.get('/api/notes');
-      setNotes(response.data);
-    } catch(error){
+      const response = await axios.get("/api/notes");
+      Array.isArray(response.data)
+        ? setNotes(response.data)
+        : setNotes([{ note: "no notes yet. this is a test note" }]);
+    } catch (error) {
       console.log(error);
+      setNotes([]);
     }
-  }
+  };
   useEffect(() => {
-  async function getNotesFromApi() {
-    await fetchNotes();
-  }
-  getNotesFromApi();
+    async function getNotesFromApi() {
+      await fetchNotes();
+    }
+    getNotesFromApi();
   }, [isSubmitting]);
 
   const handleSubmit = async event => {
     event.preventDefault();
-    setIsSubmitting(true)
+    setIsSubmitting(true);
     try {
-      await axios.post('/api/notes', {
+      await axios.post("/api/notes", {
         note: newNote
       });
-    } catch(error){
+    } catch (error) {
       console.log(error);
     } finally {
-      setNewNote('')
+      setNewNote("");
       setTimeout(() => {
-        setIsSubmitting(false)
-      }, 100)
+        setIsSubmitting(false);
+      }, 100);
     }
   };
 
   const renderAllNotes = () => {
     return (
       <div>
-        {
-          (notes || []).map(n => (
-          <div key={n.note}>{n.note}</div>
-        ))}
-      </div>
-    )
-  }
-
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Add a new note:</label>
-          <input value={newNote} onChange={(e) => setNewNote(e.target.value)}
-           />
-          <button>Submit</button>
-        </form>
-
-        <h3>All notes: </h3>
-        {renderAllNotes()}
+        {!!notes &&
+          notes.map(n => {
+            console.log(n);
+            return <div key={n.note}>{n.note}</div>;
+          })}
       </div>
     );
-}
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Add a new note:</label>
+        <input value={newNote} onChange={e => setNewNote(e.target.value)} />
+        <button>Submit</button>
+      </form>
+
+      <h3>All notes: </h3>
+      {renderAllNotes()}
+    </div>
+  );
+};
 
 export default Notes;
