4b3cd582fdf592ca88f13344b307939678c3da82
MIchal Burdzy <mmicalt@gmail.com>
2020-04-02 21:54:58 +0200

    initial config for kubernetes deployment with Travis

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Notes app v.30.03.2020</h1>
+        <h1>Notes app</h1>
+        <h2>v.02.04.2020</h2>
         <Notes />
       </div>
     );
