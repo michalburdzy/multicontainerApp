6b0ba2cdecc6edfc4ac7ab3a4b119782e5a62d12
MIchal Burdzy <mmicalt@gmail.com>
2020-04-05 14:27:53 +0200

    change docker images push order

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,3 @@
-# push :GIT_SHA images to docker hub
-docker push mmicalt/multicontainer_api_getter:$GIT_SHA
-docker push mmicalt/multicontainer_api_setter:$GIT_SHA
-docker push mmicalt/multicontainer_api_worker:$GIT_SHA
-docker push mmicalt/multicontainer_client:$GIT_SHA
-
 kubectl apply -f k8s
 kubectl set image deployments/getter-deployment getter=mmicalt/multicontainer_api_getter:$GIT_SHA
 kubectl set image deployments/setter-deployment setter=mmicalt/multicontainer_api_setter:$GIT_SHA
