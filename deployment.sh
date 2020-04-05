c360d9666c7f30329a92cea93e5eb2f78114d206
MIchal Burdzy <mmicalt@gmail.com>
2020-04-05 09:10:25 +0200

    fix v11

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,3 @@
-# build images and tag them with $GIT_SHA
-docker build -t mmicalt/multicontainer_api_getter:$GIT_SHA -f./api_getter/Dockerfile ./api_getter
-docker build -t mmicalt/multicontainer_api_setter:$GIT_SHA -f./api_setter/Dockerfile ./api_setter
-docker build -t mmicalt/multicontainer_api_worker:$GIT_SHA -f./api_worker/Dockerfile ./api_worker
-docker build -t mmicalt/multicontainer_client:$GIT_SHA -f./client/Dockerfile ./client
-
 # push :GIT_SHA images to docker hub
 docker push mmicalt/multicontainer_api_getter:$GIT_SHA
 docker push mmicalt/multicontainer_api_setter:$GIT_SHA
@@ -12,6 +6,6 @@ docker push mmicalt/multicontainer_client:$GIT_SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/getter-deployment getter=mmicalt/multicontainer_api_getter:$GIT_SHA
-kubectl set image deployments/setter-deployment getter=mmicalt/multicontainer_api_setter:$GIT_SHA
-kubectl set image deployments/worker-deployment getter=mmicalt/multicontainer_api_worker:$GIT_SHA
-kubectl set image deployments/client-deployment getter=mmicalt/multicontainer_client:$GIT_SHA
+kubectl set image deployments/setter-deployment setter=mmicalt/multicontainer_api_setter:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=mmicalt/multicontainer_api_worker:$GIT_SHA
+kubectl set image deployments/client-deployment client=mmicalt/multicontainer_client:$GIT_SHA
